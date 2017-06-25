e2694141ed496c59feb66ba773222853f7937909
Carlos Urtubey <urtubeycarlos.0510@outlook.com>
2017-06-25 14:47:39 -0300

    Agregado toString()

--- Practica1/src/ejercicio10/Estudiante.java
+++ Practica1/src/ejercicio10/Estudiante.java
@@ -1,5 +1,7 @@
 package ejercicio10;
 
+import java.util.LinkedList;
+
 public class Estudiante {
 
 	String nombre;
@@ -8,6 +10,18 @@ public class Estudiante {
 	public Estudiante(String nombre, int legajo){
 		this.nombre = nombre;
 		this.legajo = legajo;
+	}
+	
+	@Override
+	public String toString(){
+		return new String( "Estudiante{ nombre = " + this.nombre + "; legajo = " + this.legajo + " }" );
+	}
+	
+	public static void main(String[] args){
+		LinkedList<Estudiante> lista  = new LinkedList<Estudiante>();
+		lista.add( new Estudiante("Carlos", 213124) );
+		lista.add( new Estudiante("Manolo", 13413413) );
+		System.out.println( lista );
 	}
 	
 }
